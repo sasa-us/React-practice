4001949a090abcb55aa24a1b20003b63525d1427
sasa-us <sasa_luu@hotmail.com>
2020-04-02 23:05:06 -0700

    arrow funct

---
+++ practice/arrowFunc.js
@@ -0,0 +1,11 @@
+const printName = (name, age) => {
+    console.log(name, age);
+}
+
+printName('sasa-us', 22);
+
+const returnNum = (num) => {
+    return num * 2;
+}
+
+console.log(returnNum(3));
