0a1c6f768efaad892034e3774cf61f497b9f33aa
sasa-us <sasa_luu@hotmail.com>
2020-04-06 22:07:51 -0700

    review import export

---
+++ practice/export.js
@@ -0,0 +1,17 @@
+const person = {
+    name: 'max'
+}
+
+export default person
+
+
+export const basedata = 2;
+
+import person from './person.js';
+import {basedata} from './utility.js';
+
+//default export use 
+//import person from './person.js'
+
+//named export use {}
+//import {basedata} from './utitlity.js';
