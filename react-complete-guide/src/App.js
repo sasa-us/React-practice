46e7473b4fe35e0a5a5a4b9b462bef0dab83da95
sasa-us <sasa_luu@hotmail.com>
2020-04-21 06:51:10 -0700

    state review

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,14 +4,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'sasa-us', age: 6},
+      { name: 'a', age: 3},
+      { name: 'b', age: 9}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi sasa-us</h1>
         <p>sunny</p>
-        <Person name="sasa-us" age="6"/>
-        <Person name="v" age="2"> my hobbies :racing </Person>
-        <Person name="v" age="2"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> my hobbies :racing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
