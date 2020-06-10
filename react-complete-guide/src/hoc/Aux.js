08f25cc43e10b113f603aa4481f71cf8f244a5e4
sasa-us <sasa_luu@hotmail.com>
2020-06-10 15:51:32 -0700

    use aux for wrap outer jsx

---
+++ react-complete-guide/src/hoc/Aux.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// only return props.childrend
+//children is special property simply output whatever gets enterd
+//between the open and closing tag of this component
+const aux = props => props.children
+
+export default aux;
