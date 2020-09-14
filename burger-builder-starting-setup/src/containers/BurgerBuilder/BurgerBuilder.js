4d835e9d5988edddfbb44fa4e03850f5b7960bca
sasa-us <sasa_luu@hotmail.com>
2020-09-14 09:07:25 -0700

    add builder controls

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
 
 class BurgerBuilder extends Component {
     
@@ -25,7 +27,8 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Burger  ingredients={this.state.ingredients} />
                 <div>burger here</div>
-                <div>Build controls</div>
+                <BuildControls />
+                <div>Build controls here</div>
             </Aux>
         );
     }
